69d02859084d696845323ddcad7a866d27456423
DeporS <depores1337@gmail.com>
2026-01-08 19:44:36 +0100

    Fix logging in on laptop

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -2,25 +2,20 @@ import { useGoogleLogin } from '@react-oauth/google';
 import api from '../services/api';
 
 const LoginButton = ({ onLoginSuccess }) => {
-    
+
     const login = useGoogleLogin({
-    
-        flow: 'auth-code', 
-        
-        onSuccess: async (codeResponse) => {
+        onSuccess: async (tokenResponse) => {
             try {
-                
                 const res = await api.post('/auth/google/', {
-                    code: codeResponse.code, 
+                    access_token: tokenResponse.access_token,
                 });
 
                 const { access, refresh } = res.data;
                 localStorage.setItem('access_token', access);
                 localStorage.setItem('refresh_token', refresh);
                 onLoginSuccess();
-                
+
             } catch (err) {
-                
                 console.error("Login Error:", err.response?.data || err.message);
                 alert("Logowanie nieudane");
             }
