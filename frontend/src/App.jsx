a41a82242afaf52887152c07f59b3807b3b40cf0
DeporS <depores1337@gmail.com>
2026-02-06 01:56:30 +0100

    Add groups page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,104 +1,135 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import CollectionPage from './pages/CollectionPage';
-import ProfilePage from './pages/ProfilePage';
-import AddShirtFormPage from './pages/AddShirtFormPage';
-import EditShirtFormPage from './pages/EditShirtFormPage';
-import EditProfilePage from './pages/EditProfilePage';
-import HistoryPage from './pages/HistoryPage';
-import NavBar from './components/NavBar';
-import api from './services/api';
+import { useState, useEffect } from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import CollectionPage from "./pages/CollectionPage";
+import ProfilePage from "./pages/ProfilePage";
+import AddShirtFormPage from "./pages/AddShirtFormPage";
+import EditShirtFormPage from "./pages/EditShirtFormPage";
+import EditProfilePage from "./pages/EditProfilePage";
+import HistoryPage from "./pages/HistoryPage";
+import GroupsPage from "./pages/GroupsPage";
+import NavBar from "./components/NavBar";
+import api from "./services/api";
 
 import ScrollToTop from "./components/utils/ScrollTop";
-import './index.css';
+import "./index.css";
 
 function App() {
-  const [user, setUser] = useState(null); // User state
-  
-  const fetchUserData = async () => {
-    try {
-        const response = await api.get('/auth/user/');
-        setUser(response.data);
-    } catch (error) {
-        console.error("Błąd pobierania profilu:", error);
-        handleLogout();
-    }
-  };
+	const [user, setUser] = useState(null); // User state
 
-  useEffect(() => {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-        fetchUserData();
-    }
-  }, []);
+	const fetchUserData = async () => {
+		try {
+			const response = await api.get("/auth/user/");
+			setUser(response.data);
+		} catch (error) {
+			console.error("Błąd pobierania profilu:", error);
+			handleLogout();
+		}
+	};
 
-  const handleLoginSuccess = () => {
-    fetchUserData();
-  };
+	useEffect(() => {
+		const token = localStorage.getItem("access_token");
+		if (token) {
+			fetchUserData();
+		}
+	}, []);
 
-  const handleLogout = () => {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
-    setUser(null);
-  };
+	const handleLoginSuccess = () => {
+		fetchUserData();
+	};
 
-  return (
-    <Router>
-        <div>
-            <div className="sticky-top">
-                <NavBar 
-                    user={user} 
-                    onLoginSuccess={handleLoginSuccess} 
-                    onLogout={handleLogout} 
-                    refreshUser={fetchUserData} 
-                />
-            </div>
-            
-            <ScrollToTop />
+	const handleLogout = () => {
+		localStorage.removeItem("access_token");
+		localStorage.removeItem("refresh_token");
+		setUser(null);
+	};
 
-            <Routes>
-            {/* Landing Page */}
-            <Route path="/" element={<CollectionPage user={user} />} />
-        
-            {/* My Collection Page */}  
-            <Route path="/my-collection"
-                element={user ? <ProfilePage user={user} /> : <Navigate to="/" />} 
-            />
+	return (
+		<Router>
+			<div>
+				<div className="sticky-top">
+					<NavBar
+						user={user}
+						onLoginSuccess={handleLoginSuccess}
+						onLogout={handleLogout}
+						refreshUser={fetchUserData}
+					/>
+				</div>
 
-            {/* User Profile Page */}
-            <Route path="/profile/:username" 
-                element={<ProfilePage user={user} />} 
-            />
+				<ScrollToTop />
 
-            {/* Edit Profile Page */}
-            <Route 
-                    path="/profile/edit" 
-                    element={
-                        <EditProfilePage 
-                            user={user} // User logged in
-                            setUser={setUser} // Function to update user state
-                        />
-                    } 
-                />
+				<Routes>
+					{/* Landing Page */}
+					<Route path="/" element={<CollectionPage user={user} />} />
 
-            {/* Add Shirt Form Page */}
-            <Route path="/add-kit"
-                element={user ? <AddShirtFormPage /> : <Navigate to="/" />} 
-            />
+					{/* My Collection Page */}
+					<Route
+						path="/my-collection"
+						element={
+							user ? (
+								<ProfilePage user={user} />
+							) : (
+								<Navigate to="/" />
+							)
+						}
+					/>
 
-            {/* Edit Shirt Form Page */}
-            <Route path="/edit-kit/:id"
-                element={user ? <EditShirtFormPage user={user} /> : <Navigate to="/" />} 
-            />
+					{/* User Profile Page */}
+					<Route
+						path="/profile/:username"
+						element={<ProfilePage user={user} />}
+					/>
 
-            {/* History Page */}
-            <Route path="/history"
-                element={<HistoryPage user={user} />} 
-            />
-            </Routes>
-        </div>
-    </Router>
-  )
+					{/* Edit Profile Page */}
+					<Route
+						path="/profile/edit"
+						element={
+							<EditProfilePage
+								user={user} // User logged in
+								setUser={setUser} // Function to update user state
+							/>
+						}
+					/>
+
+					{/* Add Shirt Form Page */}
+					<Route
+						path="/add-kit"
+						element={
+							user ? <AddShirtFormPage /> : <Navigate to="/" />
+						}
+					/>
+
+					{/* Edit Shirt Form Page */}
+					<Route
+						path="/edit-kit/:id"
+						element={
+							user ? (
+								<EditShirtFormPage user={user} />
+							) : (
+								<Navigate to="/" />
+							)
+						}
+					/>
+
+					{/* History Page */}
+					<Route
+						path="/history"
+						element={<HistoryPage user={user} />}
+					/>
+
+					{/* Groups Page */}
+					<Route
+						path="/groups"
+						element={<GroupsPage user={user} />}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -86,14 +86,14 @@ const NavBar = ({ user, onLoginSuccess, onLogout }) => {
 						<hr className="d-lg-none text-muted w-100 my-3" />
 						<li className="nav-item">
 							<NavLink
-								to="/history"
+								to="/groups"
 								className={({ isActive }) =>
 									`nav-link px-3 fw-bold text-nowrap ${isActive ? "text-primary" : "text-dark"}`
 								}
 								onClick={closeMenu}
 								style={{ fontSize: "1.1rem" }}
 							>
-								Kit Museum
+								Groups
 							</NavLink>
 						</li>
 						{/* More links here */}
